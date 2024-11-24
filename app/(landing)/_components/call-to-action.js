e7618ad890998efb971436ca79ae5745f587f37c
Kendrick Johnson <kendrick.d3.johnson@gmail.com>
2024-11-24 10:41:26 -0500

    Style: Update hero section copy

--- app/(landing)/_components/call-to-action.js
+++ app/(landing)/_components/call-to-action.js
@@ -7,24 +7,21 @@ export const CallToAction = () => {
   return (
     <div className="flex flex-col items-start gap-y-5 md:items-center md:gap-y-5">
       <h1 className="text-left text-[35px] font-semibold leading-tight md:text-center md:text-[55px] lg:text-[70px] xl:text-[80px]">
-        Grow your email list
+        Convert your traffic
         <br />
         without being annoying.
       </h1>
       <p className="text-left text-base text-muted-foreground md:text-center">
-        Hiither turns your visitors into community members with{" "}
-        <span className="font-medium text-primary">
-          embeddable social threads
-        </span>
-        , <br className="hidden md:block" /> triggered via any button or link on
-        your website... like{" "}
+        Hiither turns your visitors into engaged community members by{" "}
         <a
           href={process.env.NEXT_PUBLIC_LP_HREF}
-          className="underline hover:text-primary hover:decoration-yellow-400"
+          className="font-medium text-primary underline hover:decoration-yellow-400"
         >
-          this one
+          leveling up your links
         </a>
         .
+        <br className="hidden md:block" /> Get started with just a single line
+        of code and a free panel to see instant results.
       </p>
       <div className="mt-5 flex w-full flex-col gap-5 md:flex-row md:justify-center">
         <Link href={process.env.NEXT_PUBLIC_LP_HREF}>
